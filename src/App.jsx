8dba85a35face037009f8eb8ad0bcb01ca21277e
godsom777 <chisom.godsom@gmail.com>
2024-03-07 02:30:13 -0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,6 @@ import Form from "./form.jsx";
 function App() {
   return (
     <div className="App">
-      <div className="app-bar">
-        <img className="logo" src="/assets/Logo.png" alt="Image 1" />
-        <img src="/assets/BGtext.png" alt="Image 2" />
-      </div>
       <h2 style={{ color: "white" }}>Please fill the Form Accordingly</h2>
       <Form />
     </div>
